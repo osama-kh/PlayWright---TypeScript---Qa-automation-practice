097acfac86039338c856034e95a0a93a285d60b6
osama-kh <75173358+osama-kh@users.noreply.github.com>
2023-11-24 17:47:12 +0200

    new add

--- wait & Timeouts/tests/test.spec.ts
+++ wait & Timeouts/tests/test.spec.ts
@@ -1,4 +1,4 @@
-import test from "playwright/test";
+import test, { expect } from "playwright/test";
 import {mainPage} from "../pages/mainPage"
 
 test("hover",async({page})=>{
@@ -11,4 +11,41 @@ await main.press_on_stat()
 // page.close()
 // main.clickOnStatistics()
     
+})
+
+
+// test('keyboard and mouse spotcheck', async ({page}) => {
+//     // Navigate to the page with the drag and drop element
+//     await page.goto('https://the-internet.herokuapp.com/hovers')
+//     // Locate the source and target elements
+//     const user =
+//          page.locator('//div[./div[./h5[contains(text(),"user1")]]]//img')
+//     // Get the bounding boxes of the elements
+//     const userBox = await user.boundingBox()
+//     // Drag the source element to the target element
+//     await page.mouse.move(userBox!.x + userBox!.width / 2,
+//         userBox!.y + userBox!.height / 2)
+//     await expect(page.locator('//a[@href="/users/1"]')).toBeVisible()
+// })
+
+
+  test.describe('Test suite with fixtures', () => {
+    test('browser fixture',async ({browser}) => {
+       const context = await browser.newContext()
+       const page = await context.newPage()
+       await page.goto('https://www.example.com')
+       const text = await page.locator('//h1').innerText()
+       expect(text).toBe('Example Domain')
+    })
+    test('browser context fixture',async ({context}) => {
+        const page = await context.newPage()
+        await page.goto('https://www.example.com')
+        const text = await page.locator('//h1').innerText()
+        expect(text).toBe('Example Domain')
+    })
+    test('page fixture', async ({page}) => {
+      await page.goto('https://www.example.com')
+      const text = await page.locator('//h1').innerText()
+      expect(text).toBe('Example Domain')
+  })
 })
